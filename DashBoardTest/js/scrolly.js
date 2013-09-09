06be4ac196aa8c4df0c54cb017706cf92f0c8790
Ryan Kelly <ryankelly23@gmail.com>
2013-09-09 16:49:24 -0400

    zcrosby first dashboard test
    First upload of zcrosby's community dashboard test. With srolly index
    saved as index

---
+++ DashBoardTest/js/scrolly.js
@@ -0,0 +1,33 @@
+var mOver = function MouseOver(){
+  /*this.style =*/
+
+  console.log("moused over!"); 
+
+}
+
+var mOut = function MouseOut(){
+  /*this.style =*/
+
+  console.log("moused out!"); 
+
+}
+
+ 
+var r = $('a');
+
+console.log(r); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$('nav').onmouseover = mOver;
+$('nav').onmouseout = mOut;*/
